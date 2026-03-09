bd1c2dfcc577ed10cc9f5f00ebc2af47d6777bca
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-03-09 11:31:30 +0100

    Added test_output for sensitivity-check

---
+++ tests/test_output/test_sensitivity_check_e.vhd
@@ -0,0 +1,11 @@
+-- Filename: test_sensitivity_check_e.vhd
+-- Created by HDL-FSM-Editor
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test_sensitivity_check is
+    port (
+        res_i : in std_logic;
+        clk_i : in std_logic 
+    );
+end entity;
