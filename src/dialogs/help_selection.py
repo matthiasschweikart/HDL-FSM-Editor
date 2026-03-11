47f414b29e571e40184b6734821389562cbef8db
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-03-11 15:40:39 +0100

    Adds additional keyboard shortcuts for text editing and selection.

--- src/dialogs/help_selection.py
+++ src/dialogs/help_selection.py
@@ -11,22 +11,28 @@ class SelectionDialog:
         content = """
 1. The key Shift-Left selects the character to the left of the insertion cursor, extending the selection.
 2. The key Shift-Right selects the character to the right of the insertion cursor, extending the selection.
-3. The key Shift-Control-Left  selects 1 word to the left.
-4. The key Shift-Control-Right  selects 1 word to the right.
-5. Dragging with mouse button 1 strokes out a selection between the insertion cursor and the character under the mouse.
-6. Double-clicking with mouse button 1 selects the word under the mouse.
-7. Dragging after a double click will stroke out a selection consisting of whole words.
-8. Triple-clicking with mouse button 1 selects the line under the mouse.
-9. Dragging after a triple click will stroke out a selection consisting of whole lines.
-10. The end of the selection can be adjusted by dragging with mouse button 1 (before the last release of a single, \
+3. The key Shift-Control-Left selects 1 word to the left.
+4. The key Shift-Control-Right selects 1 word to the right.
+5. The key Shift-Control-Up selects 1 paragraph up.
+6. The key Shift-Control-Down selects 1 paragraph down.
+7. The key Shift-Home selects from the beginning of the line to the cursor position.
+8. The key Shift-End selects from the cursor position to the end of the line.
+9. The key Ctrl-Shift-Home selects from the beginning of the text to the cursor position.
+10. The key Ctrl-Shift-End selects from the cursor position to the end of the text.
+11. Dragging with mouse button 1 strokes out a selection between the insertion cursor and the character under the mouse.
+12. Double-clicking with mouse button 1 selects the word under the mouse.
+13. Dragging after a double click will stroke out a selection consisting of whole words.
+14. Triple-clicking with mouse button 1 selects the line under the mouse.
+15. Dragging after a triple click will stroke out a selection consisting of whole lines.
+16. The end of the selection can be adjusted by dragging with mouse button 1 (before the last release of a single, \
 double or triple clicking) while the Shift key is down.
-11. The end of the selection can be adjusted character-wise when Left or Right is typed with the Shift key down.
-12. The end of the selection can be adjusted word-wise when Ctrl-Left or Ctrl-Right is typed with the Shift key down.
-13. The end of the selection can be adjusted line-wise when Up or Down is typed with the Shift key down.
-14. The end of the selection can be adjusted page-wise when PageUp or PageDown is typed with the Shift key down.
-15. Clicking mouse button 1 with the Ctrl key down will reposition the insertion cursor without affecting the selection.
-16. The Insert key inserts the selection at the position of the insertion cursor.
-17. If mouse button 2 (scroll wheel) is clicked without moving the mouse, the selection is copied into the text at the \
+17. The end of the selection can be adjusted character-wise when Left or Right is typed with the Shift key down.
+18. The end of the selection can be adjusted word-wise when Ctrl-Left or Ctrl-Right is typed with the Shift key down.
+19. The end of the selection can be adjusted line-wise when Up or Down is typed with the Shift key down.
+20. The end of the selection can be adjusted page-wise when PageUp or PageDown is typed with the Shift key down.
+21. Clicking mouse button 1 with the Ctrl key down will reposition the insertion cursor without affecting the selection.
+22. The Insert key inserts the selection at the position of the insertion cursor.
+23. If mouse button 2 (scroll wheel) is clicked without moving the mouse, the selection is copied into the text at the \
 position of the mouse cursor.
 """
-        text_dialog.TextDialog("Working with Selections", content, "1000x300")
+        text_dialog.TextDialog("Working with Selections", content, "1000x420")
--- src/dialogs/help_shortcuts.py
+++ src/dialogs/help_shortcuts.py
@@ -21,15 +21,19 @@ class ShortCutsDialog:
 7. Ctrl-Z or Ctrl-y redoes the last action.
 8. Ctrl-Left moves insertion-cursor 1 word left.
 9. Ctrl-Right moves insertion-cursor 1 word right.
-10. Ctrl-] indents the selection or the current line (works only under Linux).
-11. Ctrl-[ unindents the selection or the current line (works only under Linux).
-12. Ctrl-Backspace deletes the word before the insertion cursor.
-13. Ctrl-Delete deletes the word after the insertion cursor.
-14. Tab without selection adds blanks at the cursor position to the next multiple of four characters.
-15. Tab with selection inside a line deletes the selection and adds blanks at the cursor position to the next multiple\
-of four characters.
-16. Tab with selection spanning multiple lines indents all selected lines (independent from start and end selection).
-17. Shift-Tab unindents the selection or the current line.
-18. The Home-Button zooms the diagram to show the entire design (same as "view all").
+10. Ctrl-Up moves the insertion cursor to the beginning of the text.
+11. Ctrl-Down moves the insertion cursor to the end of the text.
+12. Ctrl-Home moves the insertion cursor to the beginning of the text.
+13. Ctrl-End moves the insertion cursor to the end of the text.
+14. Ctrl-] indents the selection or the current line (works only under Linux).
+15. Ctrl-[ unindents the selection or the current line (works only under Linux).
+16. Ctrl-Backspace deletes the word before the insertion cursor.
+17. Ctrl-Delete deletes the word after the insertion cursor.
+18. Tab without selection adds blanks at the cursor position to the next multiple of four characters.
+19. Tab with selection inside a line deletes the selection and adds blanks at the cursor position to the next multiple\
+ of four characters.
+20. Tab with selection spanning multiple lines indents all selected lines (independent from start and end selection).
+21. Shift-Tab unindents the selection or the current line.
+22. The Home-Button zooms the diagram to show the entire design (same as "view all").
 """
-        text_dialog.TextDialog("Keyboard Shortcuts for text editing", content, "1000x320")
+        text_dialog.TextDialog("Keyboard Shortcuts for text editing", content, "1000x380")
