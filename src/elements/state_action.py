ca337e61858359323a07bd72fe735def15055e2c
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-06-11 10:41:03 +0200

    Refactoring of import statements

--- src/elements/state_action.py
+++ src/elements/state_action.py
@@ -5,10 +5,10 @@ Handles the state action of all states.
 import tkinter as tk
 from tkinter import ttk
 
-import widgets.custom_text as custom_text
 from actions import canvas_delete, canvas_editing, move_handling_canvas_window
 from gui import tab_diagram
 from project_manager import project_manager
+from widgets import custom_text
 
 
 class StateAction:
