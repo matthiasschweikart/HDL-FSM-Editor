558e8505655044a17bb7258192fce33f8908554c
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-06-12 14:54:33 +0200

    Set focus to text in log_tab so that PageUp/Down works immediately

--- src/gui/compile_handling.py
+++ src/gui/compile_handling.py
@@ -42,6 +42,7 @@ def compile_hdl() -> None:
     )
     project_manager.tab_log_ref.log_frame_text.config(state=tk.DISABLED)
     project_manager.tab_log_ref.log_frame_text.see(tk.END)
+    project_manager.tab_log_ref.log_frame_text.focus_set()
     project_manager.root.update()  # Wait until the user can see the log.
     start_time = datetime.now()
     commands = _get_command_list()
