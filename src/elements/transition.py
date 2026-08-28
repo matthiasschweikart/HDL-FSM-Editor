afa04740d0671894fedc1f5c85637096317ef5f6
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-08-28 16:55:39 +0200

    Refactoring by transforming a class method into  an instance method

--- src/elements/transition.py
+++ src/elements/transition.py
@@ -285,11 +285,10 @@ class TransitionLine:
             if tag.startswith("coming_from_"):
                 start_state = tag[12:]
                 # Adapt visibility after the transition was removed:
-                TransitionLine._adapt_visibility_of_priority_rectangles_at_state(start_state)
+                self._adapt_visibility_of_priority_rectangles_at_state(start_state)
         del TransitionLine.ref_dict[self.transition_id]
 
-    @classmethod
-    def _adapt_visibility_of_priority_rectangles_at_state(cls, start_state) -> None:
+    def _adapt_visibility_of_priority_rectangles_at_state(self, start_state) -> None:
         """Hide priority rect/text for the single outgoing transition from start_state; show if multiple."""
         tags_of_start_state = project_manager.canvas.gettags(start_state)
         number_of_outgoing_transitions = 0
